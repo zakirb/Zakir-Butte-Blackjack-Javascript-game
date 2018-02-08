6d8603c7e94a1e3ecb0c95ea219a9d3fadec333c
Zakir Butte <zakir.butte@gmailcom>
2018-02-07 17:03:45 -0800

    fixed score counter, need to fix image for dealer card

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ var playerActiveCards = [];
 var dealerActiveCards = [];
 var playerTotal = 0;
 var dealerTotal = 0;
+var dealerCardImageUrl = null;
 
 var getCards = function () {$.get('https://deckofcardsapi.com/api/deck/new/draw/?count=' + (deckCount * 52).toString()).done(function(data) {
     cardArray = data.cards;
@@ -27,11 +28,18 @@ var getCards = function () {$.get('https://deckofcardsapi.com/api/deck/new/draw/
 var countTotal = function(arr) {
   	var total = 0;
 	var sum = arr.reduce(function(acc, curVal){
-	  	if (curVal[0]) {
+	  	if (acc[0] && curVal[0]) {
+	  		acc = acc[0];
+	  		curVal = curVal[0]
+	  		return acc + curVal;
+	  	} else if (curVal[0]) {
 	    	curVal = curVal[0];
 	  		return acc + curVal;
-	  	} else {
+	  	} else if (acc[0]) {
+	  		acc = acc[0];
 	    	return acc + curVal;
+	  	} else {
+	  		return acc + curVal;
 	  	}
 	});
 	
@@ -39,12 +47,19 @@ var countTotal = function(arr) {
 	
 	if (sum > 21) {
 	  	var secondSum = arr.reduce(function(acc, curVal) {
-	    	if (curVal[1]) {
-	      		curVal = curVal[1];
-	      		return acc+ curVal;
-	    	} else {
-	      		return acc + curVal;
-	    	}
+	    	if (acc[0] && curVal[0]) {
+	  			acc = acc[1];
+	  			curVal = curVal[1]
+	  			return acc + curVal;
+	  		} else if (curVal[0]) {
+	   	 		curVal = curVal[1];
+	  			return acc + curVal;
+	  		} else if (acc[0]) {
+	  			acc = acc[1];
+	   	 		return acc + curVal;
+	  		} else {
+	  			return acc + curVal;
+	  		}
 	  	});
 
 	  	total = secondSum;
@@ -64,20 +79,14 @@ var displayScore = function() {
 		playerTotal = 0;
 	}
 
-	// if (player === 1) {
-	// 	if (dealerActiveCards[1]) {
-	// 		dealerTotal = countTotal(dealerActiveCards);
-	// 	} else if (dealerActiveCards) {
-	// 		dealerTotal = cardArray[cardCount].numericalValue;
-	// 	} else {
-	// 		dealerTotal = 0;
-	// 	}	
-	// }
+	if (player === 2) {
+		if (dealerActiveCards[1]) {
+			dealerTotal = countTotal(dealerActiveCards);
+			$('#dealerscorebox').text(' | Dealer: ' + dealerTotal);
+		} 
+	} 
 
-	// playerTotal = countTotal(playerActiveCards);
-	// dealerTotal = countTotal(dealerActiveCards);
 	$('#playerscorebox').text('Player: ' + playerTotal);
-	// $('#dealerscorebox').text(' | Dealer: ' + dealerTotal);
 };
 //---------------------------------------------------
 
@@ -88,14 +97,19 @@ var checkForBust = function () {
 };
 
 var takeAStand = function () {
-
+	player = 2;
+	displayScore();
+	dealerPlays();
+	
+	
 };
 
 var dealerPlays = function () {
 	if (player === 1) {
 	if (moveCount === 1) {
 		var dealerCard = $("<img>");
-		dealerCard.attr('src', 'img/card_back.svg').addClass("cards").appendTo('#dealercards');
+		dealerCard.attr('src', 'img/card_back.svg').addClass("upsidedowncard").appendTo('#dealercards');
+		dealerCardImageUrl = cardArray[cardCount].images.svg
 		dealerActiveCards.push(cardArray[cardCount].numericalValue);
 		moveCount++;
 		cardCount++;
@@ -111,7 +125,12 @@ var dealerPlays = function () {
 	}
 	}
 
-	////finish dealer AI to automatically play out
+	if (player === 2) {
+		$('.upsidedowncard').attr('src', dealerCardImageUrl).toggleClass('cards').toggleClass('upsidedowncard');
+		////finish dealer AI to automatically play out
+	}
+
+	
 
 };
 
@@ -126,6 +145,7 @@ var dealToPlayer = function () {
 		moveCount++;
 		cardCount++;
 		player = 1;
+		displayScore();
 	} else {
 		var playerCard = $("<img>");
 		playerCard.attr('src', cardArray[cardCount].images.svg).addClass("cards").appendTo('#playercards');
@@ -149,10 +169,7 @@ var dealInitialCards = function () {
 	dealToPlayer();
 	dealerPlays();
 	$('#hitbutton').click(dealToPlayer);
-	$('#standbutton').click( function () {
-		player = 1;
-		dealerPlays();
-	});
+	$('#standbutton').click(takeAStand);
 
 	//add event listeners for for hit/stand
 	//after stand dealerPlays();
